b1f06e52a6f6151226b7c07142c25bb1acaffea9
Yago González <yagogonzalezg@gmail.com>
2021-03-18 10:23:59 +0100

    feat: Implement session storage w/ database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const register = require('@react-ssr/express/register');
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const { sequelize } = require('./db');
 const router = require('./routes');
 
 const env = process.env.NODE_ENV || 'development';
@@ -10,10 +12,14 @@ const config = require('../config.json')[env];
 const app = express();
 
 // Set up session storage.
+const sessionStore = new SequelizeStore({ db: sequelize });
+sessionStore.sync(); // Create/Sync the table in the database.
 const sessionOptions = {
+	store: sessionStore,
 	secret: config.server.sessionSecret,
 	cookie: {},
 	saveUninitialized: true,
+	resave: false,
 };
 if (env === 'production') {
 	sessionOptions.cookie.secure = true;
