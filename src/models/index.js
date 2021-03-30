628a233813e9df023d218332cf6b38736132f094
Yago González <yagogonzalezg@gmail.com>
2021-03-30 23:20:33 +0200

    feat: Output DB logs only in development

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../../config.json')[env];
 
 // Establish the database connection.
-module.exports.sequelize = new Sequelize(config.database);
+module.exports.sequelize = new Sequelize({
+	logging: env === 'development',
+	...config.database,
+});
 
 module.exports.User = require('./user');
