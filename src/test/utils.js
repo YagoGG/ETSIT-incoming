0b5056d29cdaf586cbe62bfa609e1f9c108ef876
Yago González <yagogonzalezg@gmail.com>
2021-03-31 02:42:34 +0200

    test: Add interceptor for JSX rendering

---
+++ src/test/utils.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+/**
+ * Jest cannot test both server and JSX code, so it is not possible to make a
+ * full integration test with the actual React views.
+ *
+ * This function adds a mocked JSX template engine that, instead of returning
+ * the corresponding React document, it produces a JSON with the following
+ * structure:
+ *
+ * 	{
+ * 		"view": "path/to/the/view/to/render",
+ * 		"props": { ... }
+ * 	}
+ *
+ * Where "view" is the path to the view that has invoked the template engine
+ * (just as you would put it in res.render(...)); and where "props" is an
+ * object with all the props passed to the view.
+ */
+module.exports.interceptJSXRenderProps = (app) => {
+	const viewsPath = path.join(__dirname, '../views/');
+	app.engine('jsx', (filePath, options, callback) => {
+		const {
+			settings, _locals, cache, ...props
+		} = options;
+		callback(null, {
+			view: filePath.replace(viewsPath, '').replace(/\.jsx$/, ''),
+			props,
+		});
+	});
+
+	app.set('views', viewsPath);
+	app.set('view engine', 'jsx');
+};
