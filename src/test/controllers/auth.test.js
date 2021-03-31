53943869f8fbfc7ebce4fc376d19348553852c56
Yago González <yagogonzalezg@gmail.com>
2021-03-31 02:43:20 +0200

    test: Add tests for the auth controller

---
+++ src/test/controllers/auth.test.js
@@ -0,0 +1,127 @@
+/**
+ * @jest-environment node
+ */
+const request = require('supertest');
+const bcrypt = require('bcrypt');
+const app = require('../../app');
+const { sequelize, User } = require('../../models');
+const { ErrorMessage } = require('../../utils/message');
+const { interceptJSXRenderProps } = require('../utils');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../../../config.json')[env];
+
+describe('auth controller', () => {
+	interceptJSXRenderProps(app);
+	const agent = request.agent(app);
+	const USER = {
+		email: 'user1@domain.com',
+		name: 'John Doe',
+	};
+	const USER_PASSWORD = 's3cr3t';
+
+	beforeAll(async () => {
+		USER.passwordHash = await bcrypt.hash(USER_PASSWORD, config.server.passwordSaltRounds);
+		await sequelize.sync({ force: true });
+		await User.create(USER);
+	});
+
+	it('should redirect to /login when visiting / unauthenticated', async () => {
+		await agent
+			.get('/')
+			.expect(302)
+			.expect('Location', '/login');
+	});
+
+	it('should reject missing credentials with matching message', async () => {
+		await agent
+			.post('/login')
+			.expect(200)
+			.expect((res) => {
+				const { messages } = res.body.props;
+				expect(res.body.view).toEqual('login');
+				expect(messages).toHaveLength(1);
+				expect(messages[0]).toEqual(new ErrorMessage('Missing credentials'));
+			});
+
+		await agent
+			.post('/login')
+			.send('email=')
+			.send('password=')
+			.expect(200)
+			.expect((res) => {
+				const { messages } = res.body.props;
+				expect(res.body.view).toEqual('login');
+				expect(messages).toHaveLength(1);
+				expect(messages[0]).toEqual(new ErrorMessage('Missing credentials'));
+			});
+
+		await agent
+			.post('/login')
+			.send('email=')
+			.send('password=bar')
+			.expect(200)
+			.expect((res) => {
+				const { messages } = res.body.props;
+				expect(res.body.view).toEqual('login');
+				expect(messages).toHaveLength(1);
+				expect(messages[0]).toEqual(new ErrorMessage('Missing credentials'));
+			});
+
+		await agent
+			.post('/login')
+			.send('email=foo@example.com')
+			.send('password=')
+			.expect(200)
+			.expect((res) => {
+				const { messages } = res.body.props;
+				expect(res.body.view).toEqual('login');
+				expect(messages).toHaveLength(1);
+				expect(messages[0]).toEqual(new ErrorMessage('Missing credentials'));
+			});
+	});
+
+	it('should reject invalid emails with matching message', async () => {
+		await agent
+			.post('/login')
+			.send('email=foo@example.com')
+			.send('password=bar')
+			.expect(200)
+			.expect((res) => {
+				const { messages } = res.body.props;
+				expect(res.body.view).toEqual('login');
+				expect(messages).toHaveLength(1);
+				expect(messages[0]).toEqual(new ErrorMessage('Incorrect email'));
+			});
+	});
+
+	it('should reject invalid passwords with matching message', async () => {
+		await agent
+			.post('/login')
+			.send(`email=${USER.email}`)
+			.send('password=bar')
+			.expect(200)
+			.expect((res) => {
+				const { messages } = res.body.props;
+				expect(res.body.view).toEqual('login');
+				expect(messages).toHaveLength(1);
+				expect(messages[0]).toEqual(new ErrorMessage('Incorrect password'));
+			});
+	});
+
+	it('should accept valid credentials', async () => {
+		await agent
+			.post('/login')
+			.send(`email=${USER.email}`)
+			.send(`password=${USER_PASSWORD}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.view).toEqual('index');
+				expect(res.body.props).not.toHaveProperty('messages');
+			});
+	});
+
+	afterAll(async () => {
+		await sequelize.close();
+	});
+});
