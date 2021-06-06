8ddce4e38cac49713331032540e0867260494286
Yago González <yagogonzalezg@gmail.com>
2021-06-07 00:54:18 +0200

    feat: Add roles to User model

---
+++ src/migrations/20210331143145-add-role.js
@@ -0,0 +1,16 @@
+const { User } = require('../models');
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.addColumn('Users', 'role', {
+			type: Sequelize.ENUM,
+			values: [User.STUDENT_ROLE, User.ADMIN_ROLE],
+			defaultValue: User.STUDENT_ROLE,
+			allowNull: false,
+		});
+	},
+
+	down: async (queryInterface) => {
+		await queryInterface.removeColumn('Users', 'role');
+	},
+};
