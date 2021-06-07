7ddda8919e72347d303a5ce6b575b04c59ff50a7
Yago González <yagogonzalezg@gmail.com>
2021-06-07 13:26:48 +0200

    feat: Redirect users to their dashboard depending on role

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,26 @@
 import express from 'express';
 
 import * as authController from '../controllers/auth';
+import { User } from '../models';
 
 import * as check from './checks';
 
 const router = express.Router();
 
 router.route('/')
-	.get(check.isLoggedIn, (req, res) => res.render('index', { user: req.user }));
+	.get(check.isLoggedIn, (req, res) => {
+		if (req.user.role === User.ADMIN_ROLE) {
+			res.redirect('/admin');
+		} else {
+			res.redirect('/my-application');
+		}
+	});
+
+router.route('/my-application')
+	.get(check.isLoggedIn, (req, res) => res.render('application_dashboard', { user: req.user }));
+
+router.route('/admin')
+	.get(check.isAdmin, (req, res) => res.render('admin_dashboard', { user: req.user }));
 
 router.route('/login')
 	.get((req, res) => res.render('login'))
