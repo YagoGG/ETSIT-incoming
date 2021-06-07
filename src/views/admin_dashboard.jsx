f5b8b1386cf5dcbe63416c9f46a8275b472c052a
Yago González <yagogonzalezg@gmail.com>
2021-06-07 13:26:22 +0200

    views: Implement individual admin & application dashboards

---
+++ src/views/admin_dashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+
+import { User } from '../models';
+
+export default function Index(props) {
+	const { user } = props;
+
+	return (
+		<>
+			<Row>
+				<Col className="d-flex justify-content-end">
+					<Button variant="outline-primary" href="/logout">Log out</Button>
+				</Col>
+			</Row>
+			<h1>Administration dashboard</h1>
+			<div>Hello, {user.name}!</div>
+			<p>
+				You user has the {user.role === User.ADMIN_ROLE ? 'administrator' : 'student'} role.
+			</p>
+		</>
+	);
+}
