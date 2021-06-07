cc1417343c51e5e7f4be649bd1f630e5e6bbbb2e
Yago González <yagogonzalezg@gmail.com>
2021-06-07 02:06:18 +0200

    feat: Add middleware for admin validation

--- src/routes/validators.js
+++ src/routes/validators.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
+import { User } from '../models';
+
 export function requireLoggedIn(req, res, next) {
 	if (req.user) {
 		next();
@@ -6,3 +7,14 @@ export function requireLoggedIn(req, res, next) {
 		res.redirect('/login');
 	}
 }
+
+export const requireAdmin = [
+	requireLoggedIn,
+	(req, res, next) => {
+		if (req.user.role === User.ADMIN_ROLE) {
+			next();
+		} else {
+			throw Error('not an admin');
+		}
+	},
+];
