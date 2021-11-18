b6e58f63bb121fab321fab1ae5a9b0fda66f0895
Yago González <yagogonzalezg@gmail.com>
2021-11-19 00:29:04 +0100

    feat: Add link to subject mgmt in admin settings

--- src/views/components/modals/settings_modal.jsx
+++ src/views/components/modals/settings_modal.jsx
@@ -29,6 +29,12 @@ export default function SettingsModal(props) {
 						onHide={() => setInstitutionsModal(false)}
 					/>
 				</Row>
+				<Row className="mb-3">
+					<Col sm={4} className="fw-bold">Subjects</Col>
+					<Col sm={8}>
+						<Button variant="primary" href="/admin/subjects">View details</Button>
+					</Col>
+				</Row>
 			</Modal.Body>
 		</Modal>
 	);
